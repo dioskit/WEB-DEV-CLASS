e3d2d3a286db48aae0b7e2b6527d65aba5132e99
dioskit <154680296+dioskit@users.noreply.github.com>
2024-03-21 13:21:25 +0530

    .

--- java script/javascriptDOM/javascript_DOM.js
+++ java script/javascriptDOM/javascript_DOM.js
@@ -1,5 +1,64 @@
 // DOM Manipulation
 
 // GetElementById()
-document.getElementById('main-header");
+// const title = document.getElementById('main-heading');
+// console.log(title);
 
+// //GetElementByClassName()
+
+// const listItem = document.getElementsByClassName('list-items');
+// console.log(listItem);
+
+// //getElementByTagName()
+// const container = document.getElementsByTagName('div');
+// console.log(container);
+
+// // querySelector()
+// const myContainer = document.querySelector('div');
+// console.log(myContainer);
+
+// //querySelectorAll()
+
+// const myContainer_ = document.querySelectorAll('div');
+// console.log(myContainer_);
+// const title = document.querySelector('#main-heading');
+
+//title.style.color = 'red';
+
+// console.log(title);
+
+// const listItems = documnet.querySelectorAll('list-items');
+
+// for(let i  = 0; i < listItems.length; i++){
+    
+//     listItems[i].style.fontSize = '5rem';
+
+// }
+
+// console.log(listItems);
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+//adding elements
+
+ul.append(li)
+
+//modfying text
+
+li.innerText = 'X-men';
+const li1 = document.createElement('li');
+ul.append(li1);
+li1.innerText = 'MIB';
+
+
+//modifying attributes and classes
+
+// li.setAttribute('id','main-heading');
+// li.removeAttribute('id');
+
+// const title = document.querySelector('#main-heading');
+// console.log(title.getAttribute('id'))
+li1.classList.add('list-items');
+li.classList.add('list-items'),
+console.log(li.classList.contains('list-items'));
---
+++ java script/javascriptDOM/parent_child_relation.js
@@ -0,0 +1,14 @@
+// DOM manipulation
+
+// traverse the DOM
+// parent Node Traversal
+
+const ul = document.querySelector('ul');
+
+console.log(ul.childNodes.parentNode);
+console.log(ul.parentElement);
+
+
+
+// Sibling Node Traversal
+
